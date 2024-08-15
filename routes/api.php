230ab6503a54f289394e4a856540a99331e946eb
mujahid <mujahid.iqbal@alpharages.com>
2024-08-15 20:42:22 +0500

    get files

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('webhook', [WebhookController::class, 'handle']);
 //Route::post('webhook', [WebhookController::class, 'handle']);
-//Route::post('webhook', [WebhookController::class, 'handle']);
